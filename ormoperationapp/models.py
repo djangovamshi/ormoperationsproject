a857d84d1c1bb41b1f306bb7bd8b9643b2cbc826
djangovamshi <djangovamshi420@gmail.com>
2021-10-09 22:55:02 +0530

    about models

--- ormoperationapp/models.py
+++ ormoperationapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# This Employee IS Used To Perform OrmOperations....!!
 class Employee(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
@@ -9,5 +10,5 @@ class Employee(models.Model):
     mobile = models.BigIntegerField()
     address = models.CharField(max_length=50)
 
-    def __str__(self):
+    def __str__(self):              # String Representataiuon By Default It Is Unvisuble.....!!
         return self.firstname
